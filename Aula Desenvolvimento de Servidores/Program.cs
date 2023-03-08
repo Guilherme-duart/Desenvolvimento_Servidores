974ccd01157f32ce0b5bb041dcb4823c783e9080
Guilherme-duart <duarteclebio@gmail.com>
2023-03-08 10:46:31 -0300

    Atribuição da classe pessoa

--- Aula Desenvolvimento de Servidores/Program.cs
+++ Aula Desenvolvimento de Servidores/Program.cs
@@ -5,3 +5,6 @@ Console.WriteLine("Hello, World!");
 
 Produto p1 = new Produto(1, "mouse sem fio", 59.90M);
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor() );
+
+Pessoa p2= new Pessoa(3,"Guilherme Henrique Duarte", "(16)99876542");
+Console.WriteLine(p2.getId() + p2.getNome() + " " + p2.getTelefone() );
