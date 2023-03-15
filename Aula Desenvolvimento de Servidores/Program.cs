2e915abe159f6d2ffd37dfeec35ea8bb080c5157
Guilherme-duart <duarteclebio@gmail.com>
2023-03-15 09:33:57 -0300

    criação da classe endereço

--- Aula Desenvolvimento de Servidores/Program.cs
+++ Aula Desenvolvimento de Servidores/Program.cs
@@ -1,11 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Aula_Desenvolvimento_de_Servidores;
+using ConsoleApp_Manha;
+using System.ComponentModel;
+using System.Security.Cryptography;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-Produto p1 = new Produto(1, "mouse sem fio", 59.90M);
-Console.WriteLine(p1.getDescricao() + " " + p1.getValor() );
+//Produto p1 = new Produto(1, "mouse sem fio", 59.90M);
+//Console.WriteLine(p1.getDescricao() + " " + p1.getValor() );
 
-Pessoa p2= new Pessoa(3,"Guilherme Henrique Duarte", "(16)99876542");
-Console.WriteLine(p2.getId() + p2.getNome() + " " + p2.getTelefone() );
+//cliente p2= new cliente(1,"Guilherme Henrique Duarte", "(16)99876542");
+//Console.WriteLine(p2.getId() + p2.getNome() + " " + p2.getTelefone() );
+
+Endereco e1 = new Endereco("maria mizari", "91", "13544778","sobral", "taquaritinga", "Sp");
+
+cliente c2 = new cliente(1, "Guilherme", "9767570", e1);
+cliente c3 = new cliente(2, "Eduardo", "9767570", e1);
+cliente c4 = new cliente(3, "Renata", "9767570", e1);
+cliente c5 = new cliente(4, "Augusto", "9767570", e1);
+
+Console.WriteLine(c2.getEndereco().getRua());
+c2.getEndereco().setRua("Prudente de Moraes");
 
---
+++ Aula Desenvolvimento de Servidores/cliente.cs
@@ -0,0 +1,71 @@
+﻿using ConsoleApp_Manha;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Desenvolvimento_de_Servidores
+{
+    public class cliente
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+        private Endereco Endereco;
+
+        public cliente(int id, string nome, string telefone , Endereco endereco)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+
+
+        }
+
+       public int getId()
+        {
+            return Id;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public Endereco getEndereco()
+        {
+            return Endereco;
+        }
+
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+
+        public void setEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+
+    }
+
+  
+}
